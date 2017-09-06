2d6b0168a6f4c3a735e8382dba58d7353fc1ce82
Waltermcq <waltermc@gmail.com>
2017-09-06 17:04:27 -0400

    update server.js with assignment/app.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 
+// use body parser module to parse JSON from HTTP body
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
+require("./test/app.js")(app);
+require("./assignment/app.js")(app);
 
-require ("./test/app.js")(app);
-
+//
 var port = process.env.PORT || 3000;
-
-app.listen(port);
+app.listen(port, ipaddress); // TODO ipaddress?
+// app.listen(port); leftover from assn 3?
